643ea227663274f8252816308705986aef2ccb51
Pablo García <pablogmesa00@gmail.com>
2021-12-18 02:23:50 +0100

    Recursividad funcional, falta corregir errores de ficheros o carpetas vacias

--- dirprotector.py
+++ dirprotector.py
@@ -36,7 +36,7 @@ def main():
         if "-r" in args: 
             recursively = True 
         if "lock" in args: lock(dir_path=target_dir, r=recursively)
-        elif "unlock" in args: unlock(dir_path=target_dir, r=recursively)
+        elif "unlock" in args: unlock(dir_path=target_dir)
         else: activate_shell(dir_path=target_dir)
     else: activate_shell(dir_path=target_dir)
         
@@ -53,7 +53,7 @@ def activate_shell(dir_path:Path):
                 recursively = True
             valid_command = True
             if command == 'lock': lock(dir_path, recursively)
-            elif command == 'unlock': unlock(dir_path, recursively)
+            elif command == 'unlock': unlock(dir_path)
         else:
             print(f"[!] '{command}' command doesn't exist in the program")
                 
@@ -95,12 +95,14 @@ def print_help():
         print(f"     - {command}: {info}")
         
 def clean_trash(locked_dir_path:Path):
+    rmtree(locked_dir_path)
+    os.remove(locked_dir_path.parent/hint_fname)
+
+def rmtree(parent_dir:Path):
     def del_rw(action, name, exc):
         os.chmod(name, stat.S_IWRITE)
         os.remove(name)
-    shutil.rmtree(locked_dir_path, onerror=del_rw)
-    os.remove(locked_dir_path.parent/hint_fname)
-    
+    shutil.rmtree(parent_dir, onerror=del_rw)
                 
 # ---------- COMMANDS --------- 
 def lock(dir_path:Path, r=False):
@@ -108,39 +110,58 @@ def lock(dir_path:Path, r=False):
     if is_locked(dir_path):
         print(f"[!] This directory is already locked")
         return
+    if len(os.listdir(dir_path)) == 0:
+        print(f"[!] This directory is empty")
+        return
     if r: print("[%] Locking directory recursively...")
     else: print("[%] Locking directory...")
     password = str(input(" + Choose a password: "))
     print(f" -> Password chosen: '{password}'")
     print(f"[-] Creating '{locked_dirname}' directory")
     dest_dir = Path(dir_path/(locked_dirname+f"-{get_date(path_friendly=True)}"))
-    _lock(dir_path, dest_dir, password, r=r)
+    outcome = _lock(dir_path, dest_dir, password, r=r)
+    
+    info_dir_path = dest_dir/info_dirname
+    # Guardamos el hash salteado de la password
+    pw_salt = generate_salt()
+    hashed_pw = derive(password.encode(), pw_salt)
+    pw_file_path = info_dir_path/password_fname
+    with open(pw_file_path, 'wb') as salt_file:
+        pickle.dump({hashed_pw: pw_salt}, salt_file)
     # Creamos un fichero para que el usuario pude guardar un pista de la contraseña
     hint_file_path = dir_path/hint_fname
     with open(hint_file_path, 'w') as hint_file:
         msg = "# Add a hint for your locked dir password:\n      => hint: ''"
         hint_file.write(msg)
 
-    print(f"[%] Finished -> '{dir_path}' has been locked")
+    if outcome == 0:
+        print(f"[%] Finished -> '{dir_path}' has been locked")
+    else:
+        print(f"[!] Some errors came up while locking '{dir_path}'")
 
 def _lock(dir_path:Path, dest_dir:Path, password, r=False):
     if not os.path.exists(dest_dir): os.mkdir(dest_dir)
+    dir_outcome = 0
     if r:
-        subdirectories = [name for name in os.listdir(dir_path) if os.path.isdir(name) and not locked_dirname+"-" in name]
+        subdirectories = [name for name in os.listdir(dir_path) if os.path.isdir(dir_path/name) and not locked_dirname+"-" in name]
         for subdir in subdirectories:
-            _lock(dir_path/subdir, dest_dir/subdir, password, r=r) 
-    file_names = [name for name in os.listdir(dir_path) if os.path.isfile(name)]
-    print(dir_path, dest_dir, file_names)
+            outcome = _lock(dir_path/subdir, dest_dir/subdir, password, r=r) 
+            if outcome == 0:
+                rmtree(dir_path/subdir)
+            else:
+                dir_outcome = 1
+    file_names = [name for name in os.listdir(dir_path) if os.path.isfile(dir_path/name)]
     if len(file_names) == 0: return
     salt_dict = {}
     for fname in file_names:
         src_path = dir_path/fname
-        # Movemos el fichero a la carpeta .locked
+        # Movemos el fichero a la carpeta destino
         dest_path = dest_dir/fname
         try:
             shutil.move(src_path, dest_path)
         except Exception as err:
             print(f"[!] Error moving '{fname}' into '{locked_dirname}' directory -> '{err}'")
+            dir_outcome = 1
             continue
         print(f"[-] Encrypting '{fname}'...")
         # Encriptamos el fichero
@@ -150,25 +171,22 @@ def _lock(dir_path:Path, dest_dir:Path, password, r=False):
             encrypt_file(dest_path, key)
         except Exception as err:
             print(f"[!] Error encrypting '{fname}' -> '{err}'")
+            dir_outcome = 1
         else:
             salt_dict[fname] = salt
     
     info_dir_path = dest_dir/info_dirname
     os.mkdir(info_dir_path)
-    # Guardamos el hash salteado de la password
-    pw_salt = generate_salt()
-    hashed_pw = derive(password.encode(), pw_salt)
-    pw_file_path = info_dir_path/password_fname
-    with open(pw_file_path, 'wb') as salt_file:
-        pickle.dump({hashed_pw: pw_salt}, salt_file)
 
     # Guardamos las salts utilizadas en un fichero a parte 
     salt_file_path = info_dir_path/salt_cellar_fname
     with open(salt_file_path, 'wb') as salt_file:
         pickle.dump(salt_dict, salt_file)
+        
+    return dir_outcome
  
     
-def unlock(dir_path:Path, r=False):
+def unlock(dir_path:Path):
     # Vemos si el directorio a sido encriptado antes
     if not is_locked(dir_path):
         print(f"[!] This directory hasn't been locked yet")
@@ -187,12 +205,25 @@ def unlock(dir_path:Path, r=False):
         print("[!] Incorrect password")
         return
     print("[%] Unlocking directory...")
+    outcome = _unlock(locked_dir_path, dir_path, password)
     
-    file_names = []
-    for fname in os.listdir(locked_dir_path):
-        cond2 = fname != password_fname and fname != salt_cellar_fname
-        if os.path.isfile(locked_dir_path/fname) and cond2:
-            file_names.append(fname)
+    if outcome == 0:
+        clean_trash(locked_dir_path)
+        print(f"[%] Finished -> '{dir_path}' has been unlocked")
+    else:
+        print(f"[!] Some errors came up while unlocking '{dir_path}'")
+    
+def _unlock(locked_dir_path:Path, dir_path:Path, password) -> int:
+    subdirectories = [name for name in os.listdir(locked_dir_path) if os.path.isdir(locked_dir_path/name) and name != info_dirname]
+    dir_outcome = 0
+    for subdir in subdirectories:
+        if not os.path.exists(dir_path/subdir): os.mkdir(dir_path/subdir)
+        outcome = _unlock(locked_dir_path/subdir, dir_path/subdir, password)
+        if outcome == 0:
+            rmtree(locked_dir_path/subdir)
+        else:
+            dir_outcome = 1
+    file_names = [name for name in os.listdir(locked_dir_path) if os.path.isfile(locked_dir_path/name)]
     with open(locked_dir_path/info_dirname/salt_cellar_fname, 'rb') as salt_file:
         salt_dict = pickle.load(salt_file)
     for fname in file_names:
@@ -203,6 +234,7 @@ def unlock(dir_path:Path, r=False):
             shutil.move(src_path, dest_path)
         except Exception as err:
             print(f"[!] Error moving '{fname}' into original directory -> '{err}'")
+            dir_outcome = 1
             continue
         print(f"[-] Decrypting '{fname}'...")
         # desencriptamos el fichero
@@ -212,14 +244,13 @@ def unlock(dir_path:Path, r=False):
             decrypt_file(dest_path, key)
         except Exception as err:
             print(f"[!] Error decrypting '{fname}' -> '{err}'")
+            dir_outcome = 1
         else:
             salt_dict.pop(fname)
     
-    if len(salt_dict) == 0:
-        clean_trash(locked_dir_path)
-    
-    print(f"[%] Finished -> '{dir_path}' has been unlocked")
-
+    file_names = [name for name in os.listdir(locked_dir_path) if os.path.isfile(locked_dir_path/name)]
+    if len(file_names) > 0 or dir_outcome == 1: return 1
+    return 0
 
 if __name__ == "__main__":
     try:
